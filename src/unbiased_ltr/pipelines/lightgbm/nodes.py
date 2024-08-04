8440a7d959b90ebda1bfe8a162b55b57625b50fb
yng87 <k.yanagi07@gmail.com>
2024-08-04 16:41:19 +0900

    fix

--- src/unbiased_ltr/pipelines/lightgbm/nodes.py
+++ src/unbiased_ltr/pipelines/lightgbm/nodes.py
@@ -20,7 +20,7 @@ logger = logging.getLogger(__name__)
 
 def train(
     df_train: pd.DataFrame, df_val: pd.DataFrame, model_params: dict[str, Any]
-) -> lgb.LGBMRanker:
+) -> lgb.Booster:
     df_train = sort_by_group(df_train)
     df_val = sort_by_group(df_val)
 
@@ -42,31 +42,42 @@ def train(
             "val_click_mean": y_val.mean(),
         }
     )
-    model = lgb.LGBMRanker(
-        **model_params,
-    )
-    model.fit(
+
+    ds_train = lgb.Dataset(
         X_train,
-        y_train,
+        label=y_train,
         group=group_train,
-        eval_set=[
-            (X_train, y_train),
-            (X_val, y_val),
-        ],
-        eval_group=[group_train, group_val],
-        eval_metric="ndcg",
-        eval_at=[5],
-        callbacks=[lgb.early_stopping(5), wandb_callback()],
+    )
+    ds_val = lgb.Dataset(
+        X_val,
+        label=y_val,
+        group=group_val,
     )
 
-    log_summary(model.booster_, save_model_checkpoint=False)
+    model_params.update(
+        {
+            "metric": "ndcg",
+            "ndcg_at": [5],
+        }
+    )
+
+    model = lgb.train(
+        model_params,
+        ds_train,
+        valid_sets=[ds_train, ds_val],
+        valid_names=["train", "val"],
+        callbacks=[lgb.log_evaluation(-1), lgb.early_stopping(5), wandb_callback()],
+    )
+
+    log_summary(model, save_model_checkpoint=False)
     return model
 
 
-def predict(model: lgb.LGBMRanker, df: pd.DataFrame) -> pd.DataFrame:
+def predict(model: lgb.Booster, df: pd.DataFrame) -> pd.DataFrame:
     X = get_features(df)
     y = get_unbiased_labels(df)
     group = get_group(df)
+    position = np.ones(X.shape[0])
     wandb.log({"pred_num_group": len(np.unique(group))})
-    ypred = model.predict(X)
+    ypred = model.predict(X, num_iteration=model.best_iteration)
     return pd.DataFrame({"group": group, "label": y, "prediction": ypred})
